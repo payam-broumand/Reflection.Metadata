237129825317a608554d79cb635989648ab1d0ab
Payam <payam.broumandi@gmail.com>
2021-04-08 13:11:21 +0430

    add LoadMetaData Class to get metadata types

---
+++ LoadMetaData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaData
+{
+    public class LoadMetaData
+    {
+        private Type type;
+
+        public LoadMetaData(Type type)
+        {
+            this.type = type;
+        }
+    }
+
+    public class LoadMetaData<T> : LoadMetaData
+    {
+        public LoadMetaData() : base(typeof(T))
+        {
+        }
+    }
+}
