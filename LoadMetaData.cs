9fc5a8d2ef6c3566a5e2deb516f9ddaa0b7f4682
Payam <payam.broumandi@gmail.com>
2021-04-08 13:23:04 +0430

    Add Load metadata properties to LoadMetaData class

--- LoadMetaData.cs
+++ LoadMetaData.cs
@@ -14,6 +14,35 @@ namespace MetaData
         {
             this.type = type;
         }
+
+        public string LoadProperties()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("-------------------------------------");
+            builder.AppendLine("Generic Type Information");
+            builder.AppendLine("--------------------------------------");
+            builder.AppendLine($"Name : {type.Name}");
+            builder.AppendLine($"Namespace : {type.Namespace}");
+            builder.AppendLine($"Asse : {type.Namespace}");
+            if(type.BaseType != null)
+                builder.AppendLine($"Base type : {type.BaseType}");
+            builder.AppendLine($"Is Public : {type.IsPublic}");
+            builder.AppendLine($"Is Class : {type.IsClass}");
+            builder.AppendLine($"Is Interface : {type.IsInterface}");
+            builder.AppendLine($"Is Generic : {type.IsGenericType}");
+            builder.AppendLine("----- Get Generic Args");
+            foreach (var arg in type.GetGenericArguments())
+            {
+                builder.AppendLine($"       <{arg.Name}>");
+            }
+            builder.AppendLine("----- Implemented Interfaces");
+            foreach (var inter in type.GetInterfaces())
+            {
+                builder.AppendLine($"        implemeted {inter.Name}")
+            }
+
+            return builder.ToString();
+        }
     }
 
     public class LoadMetaData<T> : LoadMetaData
